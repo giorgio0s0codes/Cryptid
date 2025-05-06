fb078ed95f7c258665cc9277194f335d99ffb273
giorgio0s0codes <giorgio.oso@outlook.com>
2025-05-05 23:19:09 -0600

    This is the preload for the JS. Safely expose specific functionality from the Electron backend .

---
+++ preload.js
@@ -0,0 +1,11 @@
+//This is the preload for the JS. 
+// Safely expose specific functionality from the Electron backend .
+
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electron', {
+    encrypt: (filePath, outputImageFileName, outputKeyFileName) => ipcRenderer.invoke('encrypt', filePath, outputImageFileName, outputKeyFileName),
+    decrypt: (filePath, keyPath, outputImageFileName) => ipcRenderer.invoke('decrypt', filePath, keyPath, outputImageFileName),
+    openFileDialog: () => ipcRenderer.invoke('open-file-dialog'),
+    saveFileDialog: (defaultPath) => ipcRenderer.invoke('save-file-dialog', defaultPath)
+});
